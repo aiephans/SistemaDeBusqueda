c3da7d09cf1f91e607a78ea1519f6517c2e41ef2
hans <hans.lopezm@correoaiep.cl>
2021-08-23 22:03:22 -0400

    Clase 20210823 - Conexion a la Bd y validacion de usuarios y campos

--- SistemaDeBusqueda/Pages/Index.cshtml.cs
+++ SistemaDeBusqueda/Pages/Index.cshtml.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using SistemaDeBusqueda.Repositories;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +12,14 @@ namespace SistemaDeBusqueda.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        [Required(ErrorMessage ="El campo usuario es requerido")]
+        public string Usuario { get; set; }
+        [Display(Name="Contraseña")]
+        [BindProperty]
+        [Required(ErrorMessage ="El campo contraseña es requerido")]
+        public string Password { get; set; }
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -20,6 +30,32 @@ namespace SistemaDeBusqueda.Pages
         public void OnGet()
         {
 
+        }
+
+        public ActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var usuario = this.Usuario;
+                var pass = this.Password;
+                var repo = new IndexRepository();
+
+                if (repo.ValidarUsuario(usuario, pass))
+                {
+                    return RedirectToPage("./Privacy");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "El usuario o la contraseña no son válidos");
+                    return Page();
+                }  
+            }
+            else
+            {
+                return Page();
+            }
+
+             
         }
     }
 }
---
+++ SistemaDeBusqueda/Repositories/IndexRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaDeBusqueda.Repositories
+{
+    public class IndexRepository
+    {
+        private readonly string ConnectionString = "server=localhost;database=sb3600db;Integrated Security=true;";
+
+        public bool ValidarUsuario(string usuario,string password)
+        {
+            bool resultado = false;
+            using SqlConnection sql = new SqlConnection(ConnectionString);
+            using SqlCommand cmd = new SqlCommand("select count(*) from usuarios where usuario = '"+ usuario + "' and [password] = '"+password+"'", sql);
+            int valor;
+
+            sql.Open();
+            valor = (int)cmd.ExecuteScalar();
+            if (valor > 0) 
+            {
+                resultado = true;
+            }
+
+            return resultado;
+        }
+
+
+    }
+}
